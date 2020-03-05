928bf1a46020def1b8b6f41ba5c002b144fa9b00
Marcin Polak <mapoart@gmail.com>
2020-03-05 11:12:23 +0100

    Added authors, developers, year..

--- windowsscripthost.win32.nexss.config.js
+++ windowsscripthost.win32.nexss.config.js
@@ -1,15 +1,17 @@
 let languageConfig = Object.assign({}, require("../config.win32"));
 languageConfig.title = "Windows Scripting Host";
 languageConfig.description = "Windows Scripting Host";
+languageConfig.founders = ["Microsoft"];
+languageConfig.developers = ["Microsoft"];
+languageConfig.years = ["1996"];
 languageConfig.url = "";
 languageConfig.extensions = [".wsf"];
-
 languageConfig.compilers = {
   WScript: {
     // install: "scoop install php",
     // Cpp does not have possibility to compile and run on the fly. We need to save it as a exe file first.
     command: "WScript.exe",
-    args: "<file>",
+    args: "/nologo <file>",
     help: ``
   }
 };
